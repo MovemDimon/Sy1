0ca045e0d44972bbe8ae07676a8ce81578da4843
MovemDimon <MovemDimon@users.noreply.github.com>
2025-04-25 05:12:04 +0000

    Apply Black auto-formatting

--- app/services/websocket.py
+++ app/services/websocket.py
@@ -1,13 +1,13 @@
 import json
 import requests
 
-RENDER_WS_URL = 'https://websocket-on-render.com/api/notify'  # لینک REST API روی WebSocket Render
+RENDER_WS_URL = (
+    "https://websocket-on-render.com/api/notify"  # لینک REST API روی WebSocket Render
+)
+
 
 def notify_user(user_id, new_balance):
-    payload = {
-        "userId": user_id,
-        "newBalance": new_balance
-    }
+    payload = {"userId": user_id, "newBalance": new_balance}
     try:
         requests.post(RENDER_WS_URL, json=payload)
     except Exception as e:
