b3131c0da8f2908552a8dc789d77ff4a9e2d6d03
MovemDimon <MovemDimon@users.noreply.github.com>
2025-05-02 08:43:11 +0000

    Apply Black auto-formatting

--- app/services/websocket.py
+++ app/services/websocket.py
@@ -6,14 +6,15 @@ import random
 WEBSOCKET_SERVERS = [
     "https://ws-server-1.fly.io/notify",
     "https://ws-server-2.fly.io/notify",
-    "https://ws-server-3.fly.io/notify"
+    "https://ws-server-3.fly.io/notify",
 ]
+
 
 def notify_user(user_id, new_balance):
     payload = {
         "userId": user_id,
         "event": "balance_updated",
-        "data": {"newBalance": new_balance}
+        "data": {"newBalance": new_balance},
     }
     try:
         selected_url = random.choice(WEBSOCKET_SERVERS)
