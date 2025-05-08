56423c7050da951694ee544d9d8260fc5e658f2c
MovemDimon <movem9013@gmail.com>
2025-05-08 12:59:24 +0330

    Create notifications.py

---
+++ app/api/v1/notifications.py
@@ -0,0 +1,20 @@
+from app.core.config import Config
+import requests
+
+def send_telegram_notification(chat_id, transaction_id, status, tx_hash=None, confirm_count=None, error_message=None):
+    """
+    ارسال وضعیت تراکنش به کاربر از طریق Bot API تلگرام.
+    """
+    text = f"تراکنش شما #{transaction_id}\nوضعیت: {status}"
+    if tx_hash:
+        text += f"\nهش: {tx_hash}"
+    if confirm_count is not None:
+        text += f"\nتعداد تأیید: {confirm_count}"
+    if error_message:
+        text += f"\nخطا: {error_message}"
+
+    payload = {
+        'chat_id': chat_id,
+        'text':    text
+    }
+    requests.post(f"{Config.TELEGRAM_API_URL}/bot{Config.TELEGRAM_TOKEN}/sendMessage", json=payload)
