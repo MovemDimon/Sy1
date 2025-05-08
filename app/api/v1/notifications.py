f63a365519a89e490acbb534e6a4a5e799d313df
MovemDimon <MovemDimon@users.noreply.github.com>
2025-05-08 09:29:55 +0000

    Apply Black auto-formatting

--- app/api/v1/notifications.py
+++ app/api/v1/notifications.py
@@ -1,7 +1,15 @@
 from app.core.config import Config
 import requests
 
-def send_telegram_notification(chat_id, transaction_id, status, tx_hash=None, confirm_count=None, error_message=None):
+
+def send_telegram_notification(
+    chat_id,
+    transaction_id,
+    status,
+    tx_hash=None,
+    confirm_count=None,
+    error_message=None,
+):
     """
     ارسال وضعیت تراکنش به کاربر از طریق Bot API تلگرام.
     """
@@ -13,8 +21,8 @@ def send_telegram_notification(chat_id, transaction_id, status, tx_hash=None, co
     if error_message:
         text += f"\nخطا: {error_message}"
 
-    payload = {
-        'chat_id': chat_id,
-        'text':    text
-    }
-    requests.post(f"{Config.TELEGRAM_API_URL}/bot{Config.TELEGRAM_TOKEN}/sendMessage", json=payload)
+    payload = {"chat_id": chat_id, "text": text}
+    requests.post(
+        f"{Config.TELEGRAM_API_URL}/bot{Config.TELEGRAM_TOKEN}/sendMessage",
+        json=payload,
+    )
